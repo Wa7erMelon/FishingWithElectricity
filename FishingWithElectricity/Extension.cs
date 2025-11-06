bcfd1d59b47f596fc8642b2cebfcb756ea854965
Wa7erMelon <hua_wu-left@outlook.com>
2025-11-06 19:17:49 +0800

    添加项目文件。

---
+++ FishingWithElectricity/Extension.cs
@@ -0,0 +1,20 @@
+﻿using Duckov.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FishingWithElectricity
+{
+	public static class Extension
+	{
+		public static string MyToString(this TagCollection tagCollection)
+		{
+			string text = "";
+			foreach (var tag in tagCollection.list)
+			{
+				text += $"{tag.name}({tag.GetInstanceID()})";
+			}
+			return text;
+		}
+	}
+}
---
+++ FishingWithElectricity/ModBehaviour.cs
@@ -0,0 +1,72 @@
+﻿using Duckov.Utilities;
+using HarmonyLib;
+using ItemStatsSystem;
+using System.Linq;
+using UnityEngine;
+using static Duckov.Utilities.GameplayDataSettings;
+
+namespace FishingWithElectricity
+{
+	public class ModBehaviour : Duckov.Modding.ModBehaviour
+	{
+		public Harmony harmony;
+
+		private void Awake()
+		{
+			Debug.Log("FishingWithElectricity Load");
+		}
+		private void OnEnable()
+		{
+			HarmonyModLoad.LoadHarmony();
+		}
+
+		private void Start()
+		{
+			harmony = new Harmony("FishingWithElectricity");
+			harmony.PatchAll();
+		}
+		private void OnDisable()
+		{
+			harmony.UnpatchAll("FishingWithElectricity");
+		}
+
+		[HarmonyPatch(typeof(Item))]
+		public static class ItemPatch
+		{
+			[HarmonyPostfix]
+			[HarmonyPatch("get_Tags")]
+			private static void AddBaitTag(Item __instance, TagCollection __result)
+			{
+				if (__instance.TypeID.Equals(942))
+				{
+					if (!__result.list.Contains(Tags.Bait))
+					{
+						__result.list.Add(Tags.Bait);
+					}
+				}
+
+				__result.list = __result.list.Distinct().ToList();
+			}
+		}
+
+		[HarmonyPatch(typeof(Action_FishingV2))]
+		public static class Action_FishingV2Patch
+		{
+			[HarmonyPrefix]
+			[HarmonyPatch("CatchFish")]
+			private static bool CatchFishPatch(Action_FishingV2 __instance, Item ___baitItem, float ___luck)
+			{
+				if (___baitItem.TypeID.Equals(942))
+				{
+					for (int i = 0; i < 9; i++)
+					{
+						Item item = __instance.lootSpawner.Spawn(___baitItem.TypeID, ___luck).GetAwaiter().GetResult();
+						__instance.characterController.PickupItem(item);
+					}
+				}
+
+				return true;
+			}
+		}
+	}
+}
