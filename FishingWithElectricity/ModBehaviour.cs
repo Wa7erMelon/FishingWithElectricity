f7b8cf1da26328e55346cf124e62c30efd9803bd
Wa7erMelon <hua_wu-left@outlook.com>
2025-11-07 22:53:43 +0800

    [Add] 新增了电鱼时音效，现在可以在电鱼时听见兹拉的声音 [Optimize] 优化了电鱼时上方通知文本，现在可以显示出所有的鱼了 [Optimize] 调整了资源，减少了Mod文件的大小

--- FishingWithElectricity/ModBehaviour.cs
+++ FishingWithElectricity/ModBehaviour.cs
@@ -1,14 +1,44 @@
-﻿using Duckov.Utilities;
+﻿using Duckov.UI;
+using Duckov.Utilities;
+using FMOD;
+using FMODUnity;
 using HarmonyLib;
 using ItemStatsSystem;
+using SodaCraft.Localizations;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using UnityEngine;
+using System.Reflection;
 using static Duckov.Utilities.GameplayDataSettings;
+using Debug = UnityEngine.Debug;
 
 namespace FishingWithElectricity
 {
 	public class ModBehaviour : Duckov.Modding.ModBehaviour
 	{
+		private const string Id = "FishingWithElectricity";
+
+		private static ChannelGroup sfxGroup;
+		public static ChannelGroup SfxGroup
+		{
+			get
+			{
+				if (!sfxGroup.hasHandle())
+				{
+					RESULT result = RuntimeManager.GetBus("bus:/Master/SFX").getChannelGroup(out sfxGroup);
+					if (result != RESULT.OK)
+					{
+						Console.WriteLine("FMOD failed to get sfx group: " + result);
+						Debug.LogError("FMOD failed to get sfx group: " + result);
+					}
+				}
+				return sfxGroup;
+			}
+		}
+
+		private static Sound electricitySound;
+
 		public Harmony harmony;
 
 		private void Awake()
@@ -18,16 +48,27 @@ namespace FishingWithElectricity
 		private void OnEnable()
 		{
 			HarmonyModLoad.LoadHarmony();
+			string soundPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Electricity.mp3"); ;
+			if (File.Exists(soundPath))
+			{
+				var soundResult = RuntimeManager.CoreSystem.createSound(soundPath, MODE.LOOP_OFF, out electricitySound);
+				if (soundResult != RESULT.OK)
+				{
+					Debug.LogError("FMOD failed to create sound: " + soundResult + "\n");
+				}
+			}
 		}
 
 		private void Start()
 		{
-			harmony = new Harmony("FishingWithElectricity");
+			harmony = new Harmony(Id);
 			harmony.PatchAll();
 		}
 		private void OnDisable()
 		{
-			harmony.UnpatchAll("FishingWithElectricity");
+			harmony.UnpatchAll(Id);
+
+			electricitySound.release();
 		}
 
 		[HarmonyPatch(typeof(Item))]
@@ -58,11 +99,31 @@ namespace FishingWithElectricity
 			{
 				if (___baitItem.TypeID.Equals(942))
 				{
-					for (int i = 0; i < 9; i++)
+					var fishDict = new List<Item>();
+
+					for (int i = 0; i < 10; i++)
 					{
 						Item item = __instance.lootSpawner.Spawn(___baitItem.TypeID, ___luck).GetAwaiter().GetResult();
+						fishDict.Add(item);
+					}
+
+					var notify = __instance.gotFishText.ToPlainText() + " " +
+									string.Join(" , ", fishDict.GroupBy(x => x.DisplayName)
+										.Select(x => $"{x.Key} x {x.Count()}")) + " !";
+					foreach (var item in fishDict)
+					{
 						__instance.characterController.PickupItem(item);
 					}
+
+					NotificationText.Push(notify);
+					RESULT result = RuntimeManager.CoreSystem.playSound(electricitySound, SfxGroup, false, out _);
+					if (!RESULT.OK.Equals(result))
+					{
+						Debug.LogError("FMOD failed to play sound: " + result + "\n");
+					}
+
+
+					return false;
 				}
 
 				return true;
